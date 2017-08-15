609f49849c661a2313900e9175d8f1131060b3b0
Nolan Brubaker <nolan@nbrubaker.com>
2017-08-14 22:00:06 -0400

    Add katakana support
    The initial loop should now allow the user to pick between katakan or
    hiragana, then proceed to the flash card game proper.
    
    Some of the provided niceties like the intro and prompt class variables
    had to be modified or moved to facilitate the differing prompts

--- shell.py
+++ shell.py
@@ -4,17 +4,31 @@ from kanamoji import KANAMOJI
 
 
 class FlashCardShell(cmd.Cmd):
-    intro = 'A basic Japanese CLI flash card tool'
-    prompt = 'your guess > '
+    prompt = '> '
 
     def initialize(self):
-        self.game = logic.FlashCardGame('hiragana', KANAMOJI)
+        self.script = ''
+        self.game = None
+        self.get_script()
+
+    def get_script(self):
+        print('A basic Japanese CLI flash card tool')
+        print('Which script would you like to practice?')
+        print('Hiragana or katakana?')
+
+    def set_script(self, arg):
+        if arg not in ('hiragana', 'katakana'):
+            print('{} was an invalid choice.')
+            self.get_script()
+        self.script = arg
+        self.game = logic.FlashCardGame(self.script, KANAMOJI)
+        self.prompt = 'your guess > '
         self.prep_question()
         self.ask()
 
     def prep_question(self):
         self.game.start_question()
-        self.prompt_char = self.game.answer[self.game.characters]
+        self.prompt_char = self.game.answer[self.script]
 
     def ask(self):
         print(self.prompt_char)
@@ -32,7 +46,10 @@ class FlashCardShell(cmd.Cmd):
             self.ask()
 
     def default(self, arg):
-        self.guess(arg)
+        if not self.game:
+            self.set_script(arg)
+        else:
+            self.guess(arg)
 
     def do_exit(self, arg):
         print('Bye')
